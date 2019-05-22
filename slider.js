6a9e4c2ad37ccf593fddb364458b0167e9f0a5d9
Logins <93darknesss93@gmail.com>
2019-05-22 18:14:56 +0400

    added

--- slider.js
+++ slider.js
@@ -1,202 +1,216 @@
 (function($){
-    
-    originCss = $.fn.css;
-    
-    $.fn.css = function() {
-        var result = originCss.apply(this, arguments);
-        $(this).trigger('stylechanged');
-        return result;
-    } 
 
-    
-    $.fn.pluginJQ  = function (options ) {   
-    
-        var settings = $.extend({
-            durationOfSlide: 3000,
-            animationSpeed : 250,
-            width: 400,
-            dotsExist : true,
-            arrowsExist : true,
-            autoslide : true,
-        }, options);
-
-
-        return this.each(function() {
-            
-            var slider = $(this);
-            var slidesQuantity = $(slider).find('li').length;
-            var video = $(slider).find('video');
-            var localContainer = slider.parent();
-
-            var width = settings.width;
-            var lastSliderWidth = (width*slidesQuantity) - width;
-            var currentSlide = 1;
-            var interval,dots,dot;
-
-            function init(){
-
-                slider.wrap('<div class="container"/>');
-                slider.find('li').css('width', settings.width);
-                localContainer.css('width', settings.width);
-
-                localContainer.on('stylechanged', function () {
-                    stopVideo();
-                });
-
-                if ( settings.dotsExist){
-                    createDots();
-                }
-
-                if ( settings.arrowsExist){
-                    addArrows();           
-                }
-
-                if ( settings.autoslide){
-                    
-                    startSlide(); 
-
-                    $('.arrows')
-                        .on('click', stopSlide)
-                        .on('click', stopVideo)
-                        .on('mouseleave',  startSlide);
-                    
-                    video
-                        .on('play',stopSlide)
-                        .on('ended',startSlide);
-                    
-                    localContainer
-                        .on('click', '.dots', stopSlide)
-                        .on('mouseleave','.dot', startSlide);
-                }
-            }
-            
-          
-            function addArrows(){
-
-                localPrev =  $("<div class='arrows prev' />").insertBefore(slider);
-                localNext = $("<div class='arrows next'/>").insertAfter(slider);
-
-                localPrev.on('click',  function(){
-                    moveSlide('left');
-                });
-                
-                localNext.on('click', function(){
-                    moveSlide('right');
-                });
-
-                localNext.css('margin-left',settings.width+10)
-
-            }
-
-            function createDots(){    
-                
-                dots = $("<div class='dots'/>").insertAfter(slider);
-              
-                for(var i = 1; i < slidesQuantity+1; i++){
-                    $('<span/>').attr('class','dot').attr('data-tag', 'd'+i).appendTo(dots);
-                }
-
-                dot = $(dots).find('.dot');
-
-                localContainer.on('click', '.dot', function(event){
-                    changeSlide(parseInt(event.target.attributes.getNamedItem('data-tag').value.split('')[1]));
-                });
-            }
+ 
+    $.fn.pluginJQ  = function (options) {
         
-            function dotChanges(currentSlide,way){
-                if ( way === 'left'){
-                    if ( currentSlide === 1){
-                        dotColorChange(slidesQuantity);
-                    } else {
-                        currentSlide--;                      
-                        dotColorChange(currentSlide);
-                    }
-                } else if( way === 'right'){
-                    dotColorChange(currentSlide);
-                    if ( currentSlide > slidesQuantity ){
-                        dotColorChange(1);
-                    } 
-                } else{
-                    throw new Error('No such way: '+way);
-                }
+        
+    var settings = $.extend({
+        dotsExist : true,
+        arrowsExist : true,
+        autoslide : true,
+        width: 400,
+        animationSpeed : 250,
+        durationOfSlide: 3000,
+    }, options);
+ 
+    function Glob(slider, settings){
+        
+        this.settings = settings;
+        this.width = this.settings.width;
+        this.slider = slider;
+        this.localContainer = this.slider.parent();
+        this.slidesQuantity = $(this.slider).find('li').length;
+        this.video = $(this.slider).find('video');
+        this.lastSliderWidth = (this.width*this.slidesQuantity) - this.width;
+        this.currentSlide = 1;
+        
+        
+        Glob.prototype.init = function (){
+            var that = this;
+
+            that.slider.wrap('<div class="container"/>');
+            that.slider.find('li').css('width', that.width);
+            that.localContainer.css('width', that.width);
+            that.localContainer.find('img').css('max-width', that.width)
+
+            if ( that.settings.dotsExist){
+                that.createDots();
             }
 
-            function dotColorChange(e){
-                --e;
-                dot.css('background-color', '#bbb');
-                $(dots.children()[e]).css('background-color', 'black');  
+            if ( that.settings.arrowsExist){
+                that.addArrows();           
             }
 
-            function startSlide () {
-                stopSlide();
-                interval = setInterval(function(){
-                    moveSlide('right');
-                }, settings.durationOfSlide);
-            }
+            if ( that.settings.autoslide){
 
-            function stopSlide(){
-                clearInterval(interval);
+                that.arrows = that.localContainer.find('.arrows');
+                that.startSlide(); 
+                
+                that.arrows
+                    .on('click', that.stopSlide)
+                    .on('click', that.stopVideo)
+                    .on('mouseleave', that.startSlide);
+                
+                this.video
+                    .on('play',that.stopSlide)
+                    .on('ended', that.startSlide);
+                
+                that.localContainer
+                    .on('click', '.dots', that.stopSlide)
+                    .on('mouseleave','.dot', that.startSlide);
             }
+        };
 
-            function updateSliderCss(value){
-                slider
-                    .stop(true,true)
-                    .animate({'margin-left' : value}, settings.animationSpeed);
-            }
+        Glob.prototype.addArrows = function (){
+            var that = this;
 
-            function changeSlide(number){
-                currentSlide = number;
-                updateSliderCss(-(number*width - width));
-                dotColorChange(number);       
-            }
+            localPrev =  $("<div class='arrows prev' />").insertBefore(that.slider);
+            localNext = $("<div class='arrows next'/>").insertAfter(that.slider);
+            localNext.css('margin-left',that.width+10)
+
+            localPrev.on('click',  function(){
+                that.moveSlide('left');
+            });
             
-            function moveSlide(way){
-                 if ( way === 'left'){
-                    if (currentSlide === 1){
-                        updateSliderCss('-='+lastSliderWidth)
-                        if( settings.dotsExist){
-                            dotChanges(currentSlide, way);
-                        }
-                        currentSlide = slidesQuantity;
-                    } else {
-                        updateSliderCss('+='+width);
-                        if( settings.dotsExist){
-                            dotChanges(currentSlide, way);
-                        }
-                        currentSlide--;
-                    }
-                } else if(way === 'right'){
-                    if ( ++currentSlide > slidesQuantity) {
-                        currentSlide = 1;
-                        updateSliderCss('0');
-                        if( settings.dotsExist){
-                            dotChanges(currentSlide, way);
-                        }
-                    } else {
-                        updateSliderCss('-='+width);
-                        if( settings.dotsExist){
-                            dotChanges(currentSlide, way);
-                        }
-                    }
-                }else{
-                    throw new Error('No such way: '+way);
-                }
-            }
-    
-            function stopVideo(){
-                if( !video.paused){                        
-                    video.trigger('pause');
-                }
+            localNext.on('click', function(){
+                that.moveSlide('right');
+            });
+        };
+
+        Glob.prototype.createDots = function (){    
+            var that = this;                    
+            that.dots = $("<div class='dots'/>").insertAfter(that.slider);
+        
+            for(var i = 1; i < that.slidesQuantity+1; i++){
+                $('<span/>').attr('class','dot').attr('data-tag', 'd'+i).appendTo(that.dots);
             }
 
+            that.dot = $(that.dots).find('.dot');
 
-            init();
-            return this;
-        });
+            that.localContainer
+            .on('click', that.stopVideo)
+            .on('click', '.dot', function(event){
+                that.changeSlide(parseInt(event.target.attributes.getNamedItem('data-tag').value.split('')[1]));
+            });
+        };
+
+        Glob.prototype.updateSliderCss = function (value){
+            var that = this;
+            that.slider
+            .stop(true,true)
+            .animate({'margin-left' : value}, that.settings.animationSpeed);
+        };
+
+        Glob.prototype.changeSlide = function (number){
+            var that = this;
+            that.currentSlide = number;
+            that.updateSliderCss(-(number*that.width -that.width));
+            that.dotColorChange(number);       
+        };
+
+        Glob.prototype.stopVideo = function (){
+            var that = this;
+            this.video.trigger('pause');
+        };
+
+        Glob.prototype.startSlide = function  () {
+            var that = this;
+            that.stopSlide();
+            that.interval = setInterval(function(){
+                that.moveSlide('right');
+            }, that.settings.durationOfSlide);
+        };
+
+        Glob.prototype.stopSlide =  function (){
+            var that = this;
+            clearInterval(that.interval);
+        };
+
+        Glob.prototype.dotColorChange = function (e){
+            var that = this;
+            --e;
+            that.dot.css('background-color', '#bbb');
+            $(that.dots.children()[e]).css('background-color', 'black');  
+        };
+
+        Glob.prototype.dotChanges = function (currentSlide,way){
+            var that = this;
+            if ( way === 'left'){
+                if ( currentSlide === 1){
+                    that.dotColorChange(that.slidesQuantity);
+                } else {
+                    currentSlide--;                      
+                    that.dotColorChange(currentSlide);
+                }
+
+            } else if( way === 'right'){
+                that.dotColorChange(currentSlide);
+                if ( currentSlide > that.slidesQuantity ){
+                    that.dotColorChange(1);
+                } 
+
+            } else{
+                throw new Error('No such way: '+way);
+            }
+        },
+
+        Glob.prototype.moveSlide = function (way){
+            var that = this;
+            if ( way === 'left'){
+                if (that.currentSlide === 1){
+                    that.updateSliderCss('-='+that.lastSliderWidth)
+                    if( that.settings.dotsExist){
+                        that.dotChanges(that.currentSlide, way);
+                    }
+                    that.currentSlide = that.slidesQuantity;
+
+                } else {
+                    that.updateSliderCss('+='+that.width);
+                    if( that.settings.dotsExist){
+                        that.dotChanges(that.currentSlide, way);
+                    }
+                    that.currentSlide--;
+                }
+
+            } else if (way === 'right'){
+                
+                if ( ++that.currentSlide > that.slidesQuantity) {
+                    that.currentSlide = 1;
+                    that.updateSliderCss('0');
+                    if( that.settings.dotsExist){
+                        that.dotChanges(that.currentSlide, way);
+                    }
+
+                } else {
+                    that.updateSliderCss('-='+that.width);
+                    if( that.settings.dotsExist){
+                        that.dotChanges(that.currentSlide, way);
+                    }
+                }
+
+            } else {
+                throw new Error('No such way: '+way);
+            }
+        };
+
     };
+    
+
+
+    return this.each(function() {
+       
+        var slider = $(this);
+        var start = new Glob(slider,settings);
+        start.init();
+
+        return this;
+    });
+
+ };
 }( jQuery ));
     
-    
+
+
 
 $(document).ready(function(){
     $('ul.slider').pluginJQ({
@@ -204,6 +218,7 @@ $(document).ready(function(){
         arrowsExist :true,
         autoslide : true,
         durationOfSlide: 1000,
+        width: 1000,
     });
 
     $('ul.slider1').pluginJQ({
@@ -212,12 +227,12 @@ $(document).ready(function(){
         autoslide : false,
         width: 700
     });
-
+    
+    
     $('ul.slider2').pluginJQ({
         dotsExist : false,
         arrowsExist :true,
-        autoslide : true,
-    });
-
-    
+        autoslide : false,
+        animationSpeed: 500
+    });  
 });
---
+++ sxa.js
@@ -0,0 +1,218 @@
+(function($){
+    
+    $.fn.pluginJQ  = function (options ) {   
+    
+        var settings = $.extend({
+            durationOfSlide: 3000,
+            animationSpeed : 250,
+            width: 400,
+            dotsExist : true,
+            arrowsExist : true,
+            autoslide : true,
+        }, options);
+
+
+        return this.each(function() {
+            
+            var slider = $(this);
+            var slidesQuantity = $(slider).find('li').length;
+            var video = $(slider).find('video');
+            var localContainer = slider.parent();
+
+            var width = settings.width;
+            var lastSliderWidth = (width*slidesQuantity) - width;
+            var currentSlide = 1;
+            var interval,dots,dot;
+
+            function init(){
+
+                slider.wrap('<div class="container"/>');
+                slider.find('li').css('width', settings.width);
+                localContainer.css('width', settings.width);
+
+             
+
+                if ( settings.dotsExist){
+                    createDots();
+                }
+
+                if ( settings.arrowsExist){
+                    addArrows();           
+                }
+
+                if ( settings.autoslide){
+
+                var arrows = localContainer.find('.arrows');
+                
+                startSlide(); 
+
+                arrows
+                    .on('click', stopSlide)
+                    .on('click', stopVideo)
+                    .on('mouseleave',  startSlide);
+                
+                video
+                    .on('play',stopSlide)
+                    .on('ended',startSlide);
+                
+                localContainer
+                    .on('click', '.dots', stopSlide)
+                    .on('mouseleave','.dot', startSlide);
+                }
+            }
+            
+          
+            function addArrows(){
+
+                localPrev =  $("<div class='arrows prev' />").insertBefore(slider);
+                localNext = $("<div class='arrows next'/>").insertAfter(slider);
+
+                localPrev.on('click',  function(){
+                    moveSlide('left');
+                });
+                
+                localNext.on('click', function(){
+                    moveSlide('right');
+                });
+
+                localNext.css('margin-left',settings.width+10)
+
+            }
+
+            function createDots(){    
+                
+                dots = $("<div class='dots'/>").insertAfter(slider);
+              
+                for(var i = 1; i < slidesQuantity+1; i++){
+                    $('<span/>').attr('class','dot').attr('data-tag', 'd'+i).appendTo(dots);
+                }
+
+                dot = $(dots).find('.dot');
+
+                localContainer
+                .on('click', stopVideo)
+                .on('click', '.dot', function(event){
+                    changeSlide(parseInt(event.target.attributes.getNamedItem('data-tag').value.split('')[1]));
+                });
+            }
+        
+            function dotChanges(currentSlide,way){
+                if ( way === 'left'){
+                    if ( currentSlide === 1){
+                        dotColorChange(slidesQuantity);
+                    } else {
+                        currentSlide--;                      
+                        dotColorChange(currentSlide);
+                    }
+                } else if( way === 'right'){
+                    dotColorChange(currentSlide);
+                    if ( currentSlide > slidesQuantity ){
+                        dotColorChange(1);
+                    } 
+                } else{
+                    throw new Error('No such way: '+way);
+                }
+            }
+
+            function dotColorChange(e){
+                --e;
+                dot.css('background-color', '#bbb');
+                $(dots.children()[e]).css('background-color', 'black');  
+            }
+
+            function startSlide () {
+                stopSlide();
+                interval = setInterval(function(){
+                    moveSlide('right');
+                }, settings.durationOfSlide);
+            }
+
+            function stopSlide(){
+                clearInterval(interval);
+            }
+
+            function updateSliderCss(value){
+                slider
+                    .stop(true,true)
+                    .animate({'margin-left' : value}, settings.animationSpeed);
+            }
+
+            function changeSlide(number){
+                currentSlide = number;
+                updateSliderCss(-(number*width - width));
+                dotColorChange(number);       
+            }
+            
+            function moveSlide(way){
+                 if ( way === 'left'){
+                    if (currentSlide === 1){
+                        updateSliderCss('-='+lastSliderWidth)
+                        if( settings.dotsExist){
+                            dotChanges(currentSlide, way);
+                        }
+                        currentSlide = slidesQuantity;
+                    } else {
+                        updateSliderCss('+='+width);
+                        if( settings.dotsExist){
+                            dotChanges(currentSlide, way);
+                        }
+                        currentSlide--;
+                    }
+                } else if(way === 'right'){
+                    if ( ++currentSlide > slidesQuantity) {
+                        currentSlide = 1;
+                        updateSliderCss('0');
+                        if( settings.dotsExist){
+                            dotChanges(currentSlide, way);
+                        }
+                    } else {
+                        updateSliderCss('-='+width);
+                        if( settings.dotsExist){
+                            dotChanges(currentSlide, way);
+                        }
+                    }
+                }else{
+                    throw new Error('No such way: '+way);
+                }
+            }
+    
+            function stopVideo(){
+                if( !video.paused){                        
+                    video.trigger('pause');
+                }
+            }
+
+
+            init();
+            return this;
+        });
+    };
+}( jQuery ));
+    
+    
+
+$(document).ready(function(){
+    $('ul.slider').pluginJQ({
+        dotsExist : true,
+        arrowsExist :true,
+        autoslide : true,
+        durationOfSlide: 1000,
+        width: 1000
+    });
+
+    $('ul.slider1').pluginJQ({
+        dotsExist : true,
+        arrowsExist :true,
+        autoslide : false,
+        width: 700
+    });
+
+    // $('ul.slider2').pluginJQ({
+    //     dotsExist : false,
+    //     arrowsExist :true,
+    //     autoslide : true,
+    //     animationSpeed: 500
+    // });
+
+    
+});
